166dc8a149a518810193237ae92ecb48dd3c1ab8
redxzeta <xzetanzero@gmail.com>
2020-12-12 16:23:47 -0800

    deleting items by a list

--- api/koku.py
+++ api/koku.py
@@ -31,3 +31,21 @@ def get_koku_categories_by_id(_id):
 @koku.route('/koku/<_id>/category/<_category_id>', methods=['GET'])
 def get_item_of_category_by_id(_id, _category_id):
     return jsonify(koku_service.get_item_of_category_by_id(_id, _category_id))
+
+
+@koku.route('/koku/<_id>/category/<_category_id>/picture', methods=['PUT'])
+def update_category_picture(_id, _category_id):
+    return jsonify(koku_service.update_category_picture(_id, _category_id))
+
+
+@koku.route('/koku/<_id>/category/<_category_id>/<update_type>', methods=['PUT'])
+def update_type(_id, _category_id, update_type):
+    key = request.get_json()[update_type]
+    print(key)
+    return jsonify(koku_service.get_kokus())
+
+
+@koku.route('/koku/<_id>/category/<_category_id>/item/remove', methods=['PUT'])
+def delete_list_of_items(_id, _category_id):
+    obj = request.get_json()
+    return jsonify(koku_service.remote_list_of_items(_id, _category_id, obj['items']))
--- service/koku.py
+++ service/koku.py
@@ -21,3 +21,24 @@ class KokuService:
 
     def get_item_of_category_by_id(self, _id, _item_id):
         return self.__koku.find_by_children(_id, _item_id)
+
+    def update_category_picture(self, _id, _category_id):
+        return self.__koku.find_by_id(_id)
+
+    def remote_list_of_items(self, _id, _category_id, item_list):
+        category_items = self.__koku.find_by_children(_id, _category_id)
+        c_items = category_items['items']
+        updated_items = []
+        for j in c_items:
+
+            if j['title'] in item_list:
+                updated_items.append(j)
+
+        category_items['items'] = updated_items
+        new_list = self.__koku.find_by_id(_id)['categories']
+        for index, k in enumerate(new_list):
+            if k['_id'] == _category_id:
+                new_list[index] = category_items
+                break
+        self.__koku.update_by_id(_id, key="categories", value=new_list)
+        return self.__koku.find_by_id(_id)
